c20e580fa161c8c02e247148ac40ea6a8f779806
Maksym Nevdashchenko <nevmaks@gmail.com>
2023-04-06 12:32:15 +0300

    Added router to application

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import React from "react";
 import GridComponent from "./grid";
 import UserDetails from "./user-details";
 import {SummaryActive, SummaryUsers} from "./summaries";
+import {Link, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
         <>
-            <GridComponent>
-                <SummaryActive />
-                <SummaryUsers />
-            </GridComponent>
-            <UserDetails />
+            <div>
+                <h1>Our awesome app</h1>
+                <ul role="navigation">
+                    <li><Link to="/grid">Grid</Link></li>
+                    <li><Link to="/details">Details</Link></li>
+                </ul>
+            </div>
+            <Routes>
+                <Route path="/grid" element={
+                    <GridComponent>
+                        <SummaryActive />
+                        <SummaryUsers />
+                    </GridComponent>
+                }/>
+                <Route exact path="/details" element ={<UserDetails/>}/>
+            </Routes>
         </>
     );
 }
